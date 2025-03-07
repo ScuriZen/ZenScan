4764ca2c80f309cb56b7913b6ae5d5313d1ada03
ScuriZen <scurizen@gmail.com>
2025-03-07 21:19:53 +0530

    sqlmap_scan.py

--- scanners/sqlmap_scan.py
+++ scanners/sqlmap_scan.py
@@ -1,6 +1,6 @@
 import os
 
 def run_sqlmap_scan(target):
-    print(f"Running SQL Injection scan on {target} using sqlmap...\n")
-    os.system(f"sqlmap -u {target} --dbs --batch")
-
+    print(f"\n\033[91m[+] Running SQL Injection scan on {target} using sqlmap...\033[0m\n")
+    output = os.popen(f"sqlmap -u {target} --dbs --batch").read()
+    return output  # Return results
