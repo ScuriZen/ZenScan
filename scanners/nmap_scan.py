214bb92003a58bc6e9cc8c61f63d22fea2423aed
ScuriZen <scurizen@gmail.com>
2025-03-07 21:19:20 +0530

    nmap_scan.py

--- scanners/nmap_scan.py
+++ scanners/nmap_scan.py
@@ -1,6 +1,6 @@
 import os
 
 def run_nmap_scan(target):
-    print(f"Running Nmap scan on {target}...")
-    os.system(f"nmap -A {target}")
-
+    print(f"\n\033[92m[+] Running Nmap scan on {target}...\033[0m\n")
+    output = os.popen(f"nmap -A {target}").read()
+    return output  # Return output to be displayed in start.py
